2ef2453f1511a81f81cb0d82a2b41fd6a2b89ec8
Leah Rieger <leastuart92@gmail.com>
2019-10-06 15:40:55 -0500

    Homepage changes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-app>
-      <Home/>
+  <v-app class="grey lighten-1">
+      <v-content> <!-- page content -->
+      <v-parallax height=1080 src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-body>Upper Echelon</v-body>
+      </v-parallax>
+    </v-content>
+    
+    <Navbar />
   </v-app>
 </template>
 
 <script>
 
-import Home from './components/Home.vue'
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'App',
-  components: {
-    Home
-  },
+  components: { Navbar },
   data () {
     return {
       //
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+    <v-toolbar flat>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-thin">Place</span>
+                <span class="font-weight-black">Holder</span>
+            </v-toolbar-title>
+    </v-toolbar>
+
+
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+        <p>test</p>
+    </v-navigation-drawer>
+    </nav>
+</template>
+
+
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: true
+        }
+    }
+}
+</script>
