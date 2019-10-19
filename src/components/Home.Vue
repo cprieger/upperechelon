50d92b4946f69d48b4dcaca9628cd5f5aab6a1ae
Leah Rieger <leastuart92@gmail.com>
2019-10-19 18:01:37 -0500

    Update

--- src/components/Home.Vue
+++ src/components/Home.Vue
@@ -3,6 +3,10 @@
     <v-parallax height=1080 src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
     <v-body>Upper Echelon</v-body>
     </v-parallax>
+    <div class="home">
+        <h1>Homepage</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam reprehenderit fugiat quibusdam alias perspiciatis tempora illum quos nesciunt fugit sit, corporis excepturi, eveniet aut dignissimos, culpa exercitationem eius sequi non.</p>
+    </div>
 </v-content>
     
 </template>
